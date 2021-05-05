a9d6b7415e05a7e53f42819449dbd4929f6a858f
LorenzoCeccarelli <56482293+LorenzoCeccarelli@users.noreply.github.com>
2021-05-05 15:20:54 +0200

    init project

---
+++ src/main/kotlin/it/polito/wa2group8/warehousemvc/WarehouseMvcApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2group8.warehousemvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarehouseMvcApplication
+
+fun main(args: Array<String>) {
+    runApplication<WarehouseMvcApplication>(*args)
+}
---
+++ src/test/kotlin/it/polito/wa2group8/warehousemvc/WarehouseMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2group8.warehousemvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarehouseMvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
