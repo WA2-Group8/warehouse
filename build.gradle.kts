2f5db3756ad41010ecde9d4c709a7b2a8e80f844
LorenzoCeccarelli <56482293+LorenzoCeccarelli@users.noreply.github.com>
2021-05-05 15:26:56 +0200

    init project

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,15 @@ repositories {
     mavenCentral()
 }
 
+allOpen{
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
