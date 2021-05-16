9b1080c49993fc33fc72164a3a35fa3d0d044f13
Riccardo Tedesco <42493739+ricky96highlander@users.noreply.github.com>
2021-05-16 16:25:14 +0200

    Fix

--- src/main/kotlin/it/polito/wa2group8/warehousemvc/WarehouseMvcApplication.kt
+++ src/main/kotlin/it/polito/wa2group8/warehousemvc/WarehouseMvcApplication.kt
@@ -1,16 +1,23 @@
 package it.polito.wa2group8.warehousemvc
 
+import it.polito.wa2group8.warehousemvc.repositories.ProductRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+
+//N.B: Not deleted because these imports are used if rows from 20 to 26 are uncommented
+import it.polito.wa2group8.warehousemvc.domain.Product
+import java.math.BigDecimal
+import kotlin.random.Random
 
 @SpringBootApplication
 class WarehouseMvcApplication
 {
-    /* UNCOMMENT TO ADD 10000 RANDOM ROWS TO THE TABLE "PRODUCT" */
-    /*
     @Bean
     fun test(productRepository: ProductRepository): CommandLineRunner {
         return CommandLineRunner{
+            /* UNCOMMENT TO ADD 10000 RANDOM ROWS TO THE TABLE "PRODUCT" */
             val categories = arrayOf<String>("Food","Electronic","Home","Sport")
             for (i in 1..10000){
                 val category = categories[Random.nextInt(0,categories.size-1)]
@@ -18,7 +25,6 @@ class WarehouseMvcApplication
             }
         }
     }
-    */
 }
 
 fun main(args: Array<String>)
